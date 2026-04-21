cc0f6c43034169067b1fc39b2b50da167ead5d48
vinoth9598 <pvinoth9598@gmail.com>
2026-04-21 11:20:00 +0530

    rendering list manual

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import Hello from "./Components/Hello";
 
 
 // Named function or function expression
-function App(){
+function App({notes}){
  
-  let userName1 = "Vinoth" ;
-  let userName2 = "Sathish";
+  console.log(notes);
   return(
     <div>
-       <h1>Greetings</h1>
-       <Hello name="vinoth" age={25}/>
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+        <li>{notes[3].content}</li>
+        <li>{notes[4].content}</li>
+      </ul>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,36 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const notes = [
+      {
+        id:1,
+        content:"Working with list",
+        important:true
+      },
+      {
+          id: 2,
+          content: 'Usage of keys',
+          important: false
+      },
+      {
+          id: 3,
+          content: 'Conditional rendering',
+          important: true
+      },
+      {
+          id: 4,
+          content: 'Handling events',
+          important: true
+      },
+      {
+          id: 5,
+          content: 'Props Vs State',
+          important: false
+      }
+
+];
 createRoot(document.getElementById('root')).render(
  
-    <App />
+    <App notes={notes}/>
   
 );
