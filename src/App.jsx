de72393ad18580d22ec7911ef08a6bec00599869
vinoth9598 <pvinoth9598@gmail.com>
2026-04-21 10:29:51 +0530

    add dynamic content

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@
 import React from "react" ;
 
 function App(){
+  const a = 10 ;
+  const b = 20 ;
+
+  const date = new Date();
   return(
     <div>
-        <p>Hello Welcome to BridgeLabz</p>
+        <p>Today Date is <b>{date.toDateString()}</b></p>
+        <p>{a} plus {b} it is {a+b}</p>
     </div>
   )
 }
